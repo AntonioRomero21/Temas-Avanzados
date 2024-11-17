200949e77523576bcf24997f7983e97a360520e7
CesatSoftware1 <romerolorenzo440@gmail.com>
2024-11-16 21:10:17 -0800

    .

--- geometry.py
+++
@@ -1,4 +0,0 @@
-def calcular_area_rectangulo(base, altura):
-    if base < 0 or altura < 0:
-        raise ValueError("La base y la altura deben ser mayores o iguales a cero.")
-    return base * altura
